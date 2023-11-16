c040c11e85183849fe29d30064f207ba7b56cc20
KYLE_ROGERS <rogerskyleart@gmail.com>
2023-11-16 00:03:50 -0500

    aidbot

---
+++ SimpleBot.java
@@ -0,0 +1,8 @@
+public class SimpleBot {
+    public static void main(String[] args) {
+        String name = "Aid";
+        String date = "2023";
+        System.out.println("Hello! My name is " + name);
+        System.out.println("I was created in " + date);
+    }
+}
