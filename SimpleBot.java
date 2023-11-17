a0278292fcb0d5f09c40a23af69a3ec1302324b2
KYLE_ROGERS <rogerskyleart@gmail.com>
2023-11-16 21:31:40 -0500

    user

--- SimpleBot.java
+++ SimpleBot.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class SimpleBot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String name = "Aid";
         String date = "2023";
         System.out.println("Hello! My name is " + name);
         System.out.println("I was created in " + date);
+        System.out.println("Please, remind me your name.");
+
+        String userName = scanner.nextLine();
+
+        System.out.println("WHat a great name you have, " + userName + " !");
+
     }
 }
